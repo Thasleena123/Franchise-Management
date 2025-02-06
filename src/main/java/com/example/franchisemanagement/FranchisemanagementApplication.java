4f3d34f1800cb68ac7f844c93b06fc6598033f92
Thasleena <thasleenaa7@gmail.com>
2025-02-06 10:35:34 +0530

    initial push

---
+++ src/main/java/com/example/franchisemanagement/FranchisemanagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.franchisemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FranchisemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FranchisemanagementApplication.class, args);
+	}
+
+}
